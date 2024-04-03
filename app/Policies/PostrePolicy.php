6c21d6f6d3a67641fd2f1e1a087e3345072c89fb
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-04-03 23:22:45 +0200

    activando las policies

--- app/Policies/PostrePolicy.php
+++ app/Policies/PostrePolicy.php
@@ -4,63 +4,62 @@ namespace App\Policies;
 
 use App\Models\Postre;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
 class PostrePolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can view any models.
+   */
+  public function viewAny(User $user): bool
+  {
+    //
+  }
 
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Postre $postre): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can view the model.
+   */
+  public function view(User $user, Postre $postre): bool
+  {
+    //
+  }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can create models.
+   */
+  public function create(User $user): bool
+  {
+    //
+  }
 
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Postre $postre): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can update the model.
+   */
+  public function update(User $user, Postre $postre): bool
+  {
+    //
+  }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Postre $postre): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can delete the model.
+   */
+  public function delete(User $user, Postre $postre): bool
+  {
+    return $user->role === 'admin';
+  }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Postre $postre): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can restore the model.
+   */
+  public function restore(User $user, Postre $postre): bool
+  {
+    //
+  }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Postre $postre): bool
-    {
-        //
-    }
+  /**
+   * Determine whether the user can permanently delete the model.
+   */
+  public function forceDelete(User $user, Postre $postre): bool
+  {
+    //
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,12 @@
 
 use App\Http\Controllers\PostreController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/crear-postre', [PostreController::class, 'store'])->name('postre.store');
+Route::get('/postres', [PostreController::class, 'index'])->name('postre.index');
 
 Route::post('register', [UserController::class, 'register'])->name('register');
 Route::post('login', [UserController::class, 'login'])->name('login');
+Route::delete('/delete/postre/{postre}', [PostreController::class, 'destroy'])->middleware('can:delete,postre')->name('postre.destroy');
